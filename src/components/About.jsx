b082ccd440d906a6edce701d3c588c6b7dd0a305
TXWZS <txwzs1103236368@gmail.com>
2026-09-20 23:53:55 +0800

    Update About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -68,18 +68,6 @@ export default function About() {
           <div className="reveal-left">
             <div className="about-photo-slot">
               <img src={`${base}assets/photo.webp`} alt="叶芳" className="about-photo-img" />
-              <div className="about-photo-overlay">
-                <a href="叶芳-27届.pdf" download="叶芳-27届.pdf" title="下载简历">
-                  <button className="about-download-circle">
-                    <span>简历</span>
-                    <svg width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
-                      <path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4" />
-                      <polyline points="7 10 12 15 17 10" />
-                      <line x1="12" y1="15" x2="12" y2="3" />
-                    </svg>
-                  </button>
-                </a>
-              </div>
             </div>
           </div>
 
